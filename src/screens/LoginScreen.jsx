d21b9a4db06e8a47d284585977b115e816e94a17
y-nagao@pro-seeds.co.jp <nagao_yuma@Nagao-Mac.local>
2022-03-30 23:30:27 +0900

    create_LoginScreen

---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, StyleSheet, Text} from 'react-native';
+import AppBar from '../components/AppBar';
+
+export default function LoginScreen() {
+    return(
+        <View>
+            <AppBar />
+        </View>
+    );
+}
