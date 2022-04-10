fa219ec3b33327a57174d20c0925f78674e52910
y-nagao@pro-seeds.co.jp <nagao_yuma@Nagao-Mac.local>
2022-04-10 10:45:13 +0900

    animation

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 
 import MemoListScreen from './src/screens/MemoListScreen';
 import MemoDetailScreen from './src/screens/MemoDetailScreen';
@@ -9,7 +9,7 @@ import MemoCreateScreen from './src/screens/MemoCreateScreen';
 import LogInScreen from './src/screens/LogInScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
     return (
@@ -22,14 +22,27 @@ export default function App() {
             headerTitle: 'Memo App',
             headerTintColor: '#fff',
             headerBackTitle: 'Back',
+            cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
           }}
         >
           <Stack.Screen name="MemoList" component={MemoListScreen} />
           <Stack.Screen name="MemoDetail" component={MemoDetailScreen} />
           <Stack.Screen name="MemoEdit" component={MemoEditScreen} />
           <Stack.Screen name="MemoCreate" component={MemoCreateScreen} />
-          <Stack.Screen name="LogIn" component={LogInScreen} />
-          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen
+            name="LogIn" 
+            component={LogInScreen}
+            options={{
+              cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+            }} 
+          />
+          <Stack.Screen 
+            name="SignUp" 
+            component={SignUpScreen} 
+            options={{
+              cardStyleInterpolator: CardStyleInterpolators.forFadeFromBottomAndroid,
+            }} 
+          />
         </Stack.Navigator>
       </NavigationContainer>
     );
